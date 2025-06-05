7ee4c48e62f783bc8c155cb847df820babb67fe7
donaldte <donaldtedom0@gmail.com>
2025-06-05 19:08:32 +0100

    feat: refactor

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,4 +1,13 @@
-import os
 import sys
+import os
 
-from jinno_site.wsgi import application
+# Add the project base directory to sys.path
+project_home = '/home/jinooca/web_application_jinoo_production/jinno_site'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Set the Django settings module (inside jinno_site/jinno_site/)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'jinno_site.settings'
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
